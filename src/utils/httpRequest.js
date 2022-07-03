ca5bddbe038b26f8bc805e05a7171ac01b99bf7f
QuangHuy111297 <dev.quanghuy1112@gmail.com>
2022-07-04 02:59:58 +0700

    changeAPI

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const httpRequest = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
+    baseURL: 'https://tiktok.fullstack.edu.vn/api/' || process.env.REACT_APP_BASE_URL,
 });
 
 export const get = async (path, options = {}) => {
